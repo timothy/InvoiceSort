0e687dfcfdc5b1128d1fe2e0351372c65b1c2fe0
Timothy <tabradford@liberty.edu>
2017-04-05 15:23:37 -0400

    Added new sort methods, cleaned up and added javaDocs.

--- src/invoicesort/Invoice.java
+++ src/invoicesort/Invoice.java
@@ -16,6 +16,13 @@ public class Invoice {
     private Double Price;
     private String PartDescription;
 
+    /**
+     * 
+     * @param PartNumber The part number
+     * @param Quantity The quantity of the order
+     * @param Price The price of each unit
+     * @param PartDescription the description of each item
+     */
     public Invoice(int PartNumber, int Quantity, double Price, String PartDescription) {
         this.PartDescription = PartDescription;
         this.PartNumber = PartNumber;
@@ -23,6 +30,10 @@ public class Invoice {
         this.Price = Price;
     }
 
+    /**
+     * 
+     * @return a formatted string of all variables
+     */
     @Override
     public String toString() {
         return "PartNumber:" + this.PartNumber + "\n"
@@ -31,34 +42,66 @@ public class Invoice {
                 + "PartDescription:" + this.PartDescription + "\n";
     }
 
+    /**
+     * 
+     * @return The part number
+     */
     public Integer getPartNumber() {
         return PartNumber;
     }
 
+    /**
+     * 
+     * @param PartNumber The part number
+     */
     public void setPartNumber(Integer PartNumber) {
         this.PartNumber = PartNumber;
     }
 
+    /**
+     * 
+     * @return The quantity of the order
+     */
     public Integer getQuantity() {
         return Quantity;
     }
 
+    /**
+     * 
+     * @param Quantity The quantity of the order
+     */
     public void setQuantity(Integer Quantity) {
         this.Quantity = Quantity;
     }
 
+    /**
+     * 
+     * @return The price of each unit
+     */
     public Double getPrice() {
         return Price;
     }
 
+    /**
+     * 
+     * @param Price The price of each unit
+     */
     public void setPrice(Double Price) {
         this.Price = Price;
     }
 
+    /**
+     * 
+     * @return the description of each item 
+     */
     public String getPartDescription() {
         return PartDescription;
     }
 
+    /**
+     * 
+     * @param PartDescription the description of each item
+     */
     public void setPartDescription(String PartDescription) {
         this.PartDescription = PartDescription;
     }
--- src/invoicesort/InvoiceSort.java
+++ src/invoicesort/InvoiceSort.java
@@ -10,6 +10,7 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
+ * https://zeroturnaround.com/rebellabs/java-8-explained-applying-lambdas-to-java-collections/
  * https://www.leveluplunch.com/java/tutorials/007-sort-arraylist-stream-of-objects-in-java8/
  *
  * @author tim
@@ -34,44 +35,62 @@ public class InvoiceSort {
 
         //Sort by Price
         Comparator<Invoice> PriceSort = (e1, e2) -> Double.compare(
-                e1.getQuantity(), e2.getQuantity());
+                e1.getPrice(), e2.getPrice());
+
+        //Sort by Value
+        Comparator<Invoice> ValueSort = (e1, e2) -> Double.compare(
+                e1.getQuantity() * e1.getPrice(), e2.getQuantity() * e2.getPrice());
 
         //Sort by PartDescription
         Comparator<Invoice> PartDescriptionSort = (e1, e2) -> e1.getPartDescription()
                 .compareTo(e2.getPartDescription());
 
         //Create a list of invoice objects
-        List<Invoice> invoice = new ArrayList<>();
-        invoice.add(new Invoice(123456, 56, 108.50, "Cars"));
-        invoice.add(new Invoice(545555, 27, 109.50, "Boats"));
-        invoice.add(new Invoice(777777, 15, 120.50, "Planes"));
-        invoice.add(new Invoice(999999, 32, 115.50, "Spoons"));
-        invoice.add(new Invoice(121212, 45, 172.50, "Bikes"));
-        invoice.add(new Invoice(151515, 27, 135.50, "Dirt"));
-        invoice.add(new Invoice(989898, 57, 194.50, "ARIfot"));
-        invoice.add(new Invoice(829155, 58, 258.50, "Bats"));
-        invoice.add(new Invoice(333333, 59, 137.50, "Shoes"));
-        invoice.add(new Invoice(111111, 70, 129.50, "Food"));
+        List<Invoice> invoiceList = new ArrayList<>();
+        invoiceList.add(new Invoice(123456, 56, 108.50, "Cars"));
+        invoiceList.add(new Invoice(545555, 27, 11.50, "Boats"));
+        invoiceList.add(new Invoice(777777, 15, 13.50, "Planes"));
+        invoiceList.add(new Invoice(999999, 32, 115.50, "Spoons"));
+        invoiceList.add(new Invoice(121212, 45, 172.50, "Bikes"));
+        invoiceList.add(new Invoice(151515, 27, 2.50, "Dirt"));
+        invoiceList.add(new Invoice(989898, 57, 194.50, "ARIfot"));
+        invoiceList.add(new Invoice(829155, 58, 1.50, "Bats"));
+        invoiceList.add(new Invoice(333333, 59, 10.50, "Shoes"));
+        invoiceList.add(new Invoice(111111, 70, 129.50, "Food"));
 
-        //Sort by PartNumber using a comparitor function
-        invoice.stream().sorted(PartNumberSort)
+        System.out.println("a. Sort the Invoice objects by PartDescription, then display the results.");
+//        //a. Sort the Invoice objects by PartDescription, then display the results.
+        invoiceList.stream().sorted(PartDescriptionSort)
+                .forEach(e -> System.out.println(e));
+        
+        System.out.println("\n\nb. Sort the Invoice objects by Price, then display the results.");
+//        //b. Sort the Invoice objects by Price, then display the results.
+        invoiceList.stream().sorted(PriceSort)
                 .forEach(e -> System.out.println(e));
 
-        //Sort by Quantity using a comparitor function
-        invoice.stream().sorted(QuantitySort)
+                System.out.println("\n\nc. Map each Invoice to its PartDescription and Quantity, sort the results by Quantity, then display the results.");
+        //c. Map each Invoice to its PartDescription and Quantity, sort the results by Quantity, then display the results.
+        invoiceList
+                .stream()
+                .sorted(QuantitySort)
+                .map(s -> s.getPartDescription() + s.getQuantity())
                 .forEach(e -> System.out.println(e));
 
-        //Sort by Price using a comparitor function
-        invoice.stream().sorted(PriceSort)
+        System.out.println("\n\nd. Map each Invoice to its PartDescription and the value of the Invoice (i.e., Quantity * Price). Order the results by Invoice value.");
+        //d. Map each Invoice to its PartDescription and the value of the Invoice (i.e., Quantity * Price). Order the results by Invoice value.
+        invoiceList
+                .stream()
+                .sorted(ValueSort)
+                .map(s -> s.getPartDescription() + (s.getQuantity() * s.getPrice()))
                 .forEach(e -> System.out.println(e));
 
-        //Sort by PartDescription using a comparitor function
-        invoice.stream().sorted(PartDescriptionSort)
+        System.out.println("\n\ne. Modify Part (d) above to select the Invoice values in the range of $200 to $500 (inclusive) and display the results separately.");
+        //e. Modify Part (d) above to select the Invoice values in the range of $200 to $500 (inclusive) and display the results separately.
+        invoiceList
+                .stream().filter(s -> 200 < s.getQuantity() * s.getPrice() && 500 > s.getQuantity() * s.getPrice())
+                .sorted(ValueSort)
+                .map(s -> s.getPartDescription() + (s.getQuantity() * s.getPrice()))
                 .forEach(e -> System.out.println(e));
 
-        //Sort by PartDescription without using a comparitor
-//        invoice.stream().sorted((e1, e2) -> e1.getPartDescription()
-//                    .compareTo(e2.getPartDescription()))
-//            .forEach(e -> System.out.println(e));
     }
 }
